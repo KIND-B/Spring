9b58dee4ab21254358aa676ba42dc42d42fae711
Dhanushka <softdp1.sdu@ci.lk>
2020-07-31 17:21:18 +0530

    add new file

---
+++ projectTest/src/main/java/com/projectTest/example/projectTest/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.projectTest.example.projectTest.service;
+
+import com.projectTest.example.projectTest.dto.request.StudentRequest;
+import com.projectTest.example.projectTest.dto.request.studentUpdateRequest;
+import com.projectTest.example.projectTest.dto.response.studentUpdateResponse;
+import com.projectTest.example.projectTest.entity.StudentsEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StudentService {
+
+    StudentsEntity save(StudentRequest studentRequest);
+
+    studentUpdateResponse update(studentUpdateRequest studentUpdateRequest);
+}
